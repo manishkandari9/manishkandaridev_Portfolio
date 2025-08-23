c0898dc0e02db868639eda0d34988928aef1c760
manish_singh <manishkandari43@gmail.com>
2025-08-23 11:10:46 +0530

    add chat bot agent

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://manishkandaridev-portfolio.vercel.app', // apna domain
+  generateRobotsTxt: true, // robots.txt bhi banega
+  sitemapSize: 7000, // optional
+};
