65492fa94ddd07c6f62e19e571f9bbe8ad6b3329
manish_singh <manishkandari43@gmail.com>
2025-08-24 15:10:15 +0530

    add build

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -6,9 +6,24 @@ module.exports = {
   sitemapSize: 5000,
   additionalPaths: async (config) => [
     {
-      loc: 'https://face-recognition-based-attendance-system-z5gk.onrender.com',
+      loc: 'https://manishkandaridev-portfolio.vercel.app/',
+      changefreq: 'daily',
+      priority: 0.7,
+    },
+    {
+      loc: 'https://manishkandaridev-portfolio.vercel.app/projects',
+      changefreq: 'daily',
+      priority: 0.9,
+    },
+    {
+      loc: 'https://manishkandaridev-portfolio.vercel.app/feedback',
       changefreq: 'weekly',
-      priority: 0.8,
+      priority: 0.7,
+    },
+    {
+      loc: 'https://manishkandaridev-portfolio.vercel.app/about',
+      changefreq: 'weekly',
+      priority: 0.7,
     },
   ],
   robotsTxtOptions: {
